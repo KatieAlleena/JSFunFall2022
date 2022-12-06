519db46b718f0fde71e936e95700db93f800d1bf
Katie Morton <kmalleena@gmail.com>
2022-12-05 20:03:09 -0500

    in class exercises done

--- exercises/19-click-events/click-events.js
+++ exercises/19-click-events/click-events.js
@@ -9,6 +9,11 @@
    * @see https://developer.mozilla.org/en-US/docs/Web/API/Window/alert
    */
   // Write your answer here
+  const button1 = document.querySelector('#alert1');
+  button1.addEventListener("click", () => {
+    alert("Alert Me!");
+  });
+
   /**
    * Problem 2: Disable a button that will charge a credit card.
    *
@@ -17,4 +22,9 @@
    * 2. Change the text to say e.g. "Loading ..." once it is clicked.
    */
   // Write your answer here
+  const button2 = document.querySelector('#loading1');
+  button2.addEventListener("click", () => {
+    button2.disabled = 'disabled';
+    button2.textContent = 'Loading...';
+  })
 })();
